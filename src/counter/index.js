189eb037240c8e865d0ea9553bff880f63469504
Jeremy Lund <jeremylund@workfront.com>
2016-09-21 21:06:17 -0600

    feat: clean up counter

--- src/counter/index.js
+++ src/counter/index.js
@@ -5,17 +5,33 @@ const btnIncrement = qs('.counter--increment');
 const btnDecrement = qs('.counter--decrement');
 const valueElement = qs('.counter--value');
 
-// 37 left, 38 up, 39 right, 40 down
+// Creates a keydown stream, mapping events to keyCode values.
+const keydown$ = Observable.fromEvent(document.body, 'keydown')
+  .map(ev => ev.keyCode); // map to keyCode values.
+
+// Uncomment to use the arrow keys to increment/decrement counters.
+// up: increase by 5
+// down: decrease by 5
+// left: decrease by 1
+// right: increatse by 1
+// const up$ = keydown$.filter(keyCode => keyCode === 38).mapTo(5);
+// const down$ = keydown$.filter(keyCode => keyCode === 40).mapTo(-5);
+// const left$ = keydown$.filter(keyCode => keyCode === 37).mapTo(-1);
+// const right$ = keydown$.filter(keyCode => keyCode === 39).mapTo(1);
 const increment$ = Observable.fromEvent(btnIncrement, 'click')
   .map(() => 1);
 const decrement$ = Observable.fromEvent(btnDecrement, 'click')
   .map(() => -1);
 
 const counter$ = Observable.merge(
+  // up$,
+  // down$,
+  // left$,
+  // right$,
   increment$,
   decrement$
 )
-  .startWith(0)
+  .startWith(0) // set a starting count value.
   .scan((total, delta) => total + delta);
 
 counter$.subscribe(value => {
